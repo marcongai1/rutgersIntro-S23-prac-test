f65afe424a9d9164a7383983ac761717d74f90ae
Marco <128193445+marcongai1@users.noreply.github.com>
2023-09-27 19:31:32 -0400

    Add files via upload

---
+++ rutgersIntro.java
@@ -0,0 +1,74 @@
+public class rutgersIntro {
+    
+    public rutgersIntro(){
+    }  
+    public static void main(String[] args){
+        rutgersIntro app = new rutgersIntro();
+    }
+}
+/* Question 1
+ * a) Should initially check if a and b are positive integers
+ * by using an if statement asking if it is greater than 0.
+ * if b is equal to 0 should print that a is not divisible by b.
+ * Pseudocode:
+ * 1. Read a
+    2. Read b
+    3. if b = 0
+    4.    Print "error"
+    5. else if a % b = 0
+    6.    Print "a is divisible by b"
+    7. else
+    8.    Print "a is not divisible by b"
+ * b) its just an input output 
+ * c) the maximum number of operations executed in the program would be 3
+ * d) the minimum would be 1
+ * 
+ * Question 2
+ *  Read n
+ *  IF n <= 0 THEN
+ *      DISPLAY "Input must be a positive int"
+ *  ELSE
+ *    SET SUM to 0
+ *    FOR i FROM 1 to n
+ *    SET SUM to SUM + i
+ *   END FOR
+ * END IF
+ * DISPLAY SUM
+ * 
+ * Question 3
+ * a) the maximum number of operations executed in the program would be
+ *  ADD 1 To count
+ * COMPUTE num AS num/10
+ * The max should be 2n. 2 iterations per digit
+ *  
+ * Section 2:
+ * Question 1
+ * c) 10
+ * 
+ * Quesiton 2:
+ * a) num1 = 5, num2 = 5
+ * c) num1 = 8, num2 = 5
+ * 
+ * Question 3:
+ * a) 11
+ * b) 39
+ * 
+ * Question 4:
+ * 2000: true
+ * 2026: false
+ * 2016: true
+ * 
+ * Question 5:
+ *  xxxxx --> n/d + " " + n%d + "/" + d
+ * 
+ * Question 6:
+ * A) int, 5
+ * B) double, 4.0
+ * C) String, 151
+ * D) String, 245
+ * E) illegal, X
+ * F) int, 6
+ * G) boolean, false
+ * H) boolean, true
+ */
+
